e233be838fc4b24b503ad3080506b790c1315540
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-02-11 23:06:41 -0500

    adds init user session context

--- src/components/MainNav/index.tsx
+++ src/components/MainNav/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import { useUserSession } from "../../contexts/user-session";
 import "./styles.css";
 
 interface IProps {
@@ -7,14 +8,30 @@ interface IProps {
 }
 
 export const MainNav: FC<IProps> = ({ className }) => {
+  const userSession = useUserSession()!;
+
+  const renderRightContent = () => {
+    if (userSession.isSignedIn) {
+      return (
+        <button className='sign-out'>
+          Sign Out
+        </button>
+      );
+    }
+
+    return (
+      <Link to='/sign-in'>
+        Sign In
+      </Link>
+    )
+  }
+
   return (
     <nav className={`main-nav ${className}`}>
       <Link to='/' className='logo'>
         Xample
       </Link>
-      <Link to='/signin'>
-        Sign In
-      </Link>
+      { renderRightContent() }
     </nav>
   );
 };
